64d69454607af61edb2ee6dd34f94660ea2ff3fb
TakenouchiTR <draconianlds@gmail.com>
2025-01-29 00:00:09 -0500

    Add go to wiki button to popup

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+import { OPTIONS_DOMAIN_EXCLUSIONS, PAGES_DB_JSON_URL } from "./constants.js";
+import { extractMainDomain, isDomainExcluded, getOptions, openForegroundTab, getPagesForDomain, fetchJson } from "./utils.js";
+
 let appDisabled = false;
 
 // get initial appDisabled
@@ -10,9 +13,11 @@ chrome.storage.sync.get(
     },
 );
 
-const buttonEle = document.getElementById("on-off-toggle");
+const onOffButtonEle = document.getElementById("on-off-toggle");
+const optionsButtonEle = document.getElementById("optionsButton");
+const wikiButtonEle = document.getElementById("wikiButton");
 
-buttonEle.addEventListener("click", () => {
+onOffButtonEle.addEventListener("click", () => {
     const newAppDisabled = !appDisabled;
     appDisabled = newAppDisabled;
 
@@ -20,6 +25,38 @@ buttonEle.addEventListener("click", () => {
     chrome.runtime.sendMessage({ badgeText: newAppDisabled ? "off" : "on" });
 });
 
-document.getElementById("optionsButton").addEventListener("click", () => {
+optionsButtonEle.addEventListener("click", () => {
     chrome.runtime.openOptionsPage();
+});
+
+chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    if (tabs.length === 0) {
+        return
+    };
+
+    const options = await getOptions([OPTIONS_DOMAIN_EXCLUSIONS, "appDisabled"]);
+
+    const activeTab = tabs[0];
+    const currentDomain = new URL(activeTab.url).hostname;
+
+    if (!currentDomain || isDomainExcluded(options.domain_exclusions, currentDomain)) {
+        return;
+    }
+    
+    const mainDomain = extractMainDomain(currentDomain);
+    
+    let pagesDB = [];
+    await fetchJson(PAGES_DB_JSON_URL).then(result => {pagesDB = result});
+
+    getPagesForDomain(mainDomain, pagesDB).then((results) => {
+        if (results.numPages === 0) {
+            return;
+        }
+        console.log("pageUrl: ", results.pageUrls[0]);
+        wikiButtonEle.addEventListener("click", () => {
+            openForegroundTab(results.pageUrls[0]);
+            window.close();
+        })
+        wikiButtonEle.disabled = false;
+    });
 });
